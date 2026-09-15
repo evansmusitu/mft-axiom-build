e92232e3653793fc3a12e0dccd0a73c1e5c64774
evansmusitu <evansmusitu5@gmail.com>
2026-09-15 12:29:05 +0200

    feat(axiom): wire FA-13 Engineering Command Center UI

--- axiom_interface/vnext/agent_runtime_ui.js
+++ axiom_interface/vnext/agent_runtime_ui.js
@@ -1,6 +1,7 @@
 import './deep_context_ui.js';
 import './mission_control_ui.js';
 import './execution_ui.js';
+import './engineering_command_center_ui.js';
 import {ProjectStore} from '../projects.js';
 import {ObservabilityStore} from '../observability.js';
 import {AgentAutomationStore} from '../agent_store.js';
